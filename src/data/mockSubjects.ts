0bb88d901027edcd49b05ae3c1c2ff0b68de6cee
Fikri Raihan <fikri.raihan4@gmail.com>
2026-01-20 14:27:19 +0700

    Add subject management features and UI components
    - Introduced new constants for departments and mock subjects.
    - Implemented a subjects list page with search and filter functionality.
    - Created a data provider for mock subjects.
    - Added new styles for various UI components in App.css.
    - Removed unused test page to clean up the project structure.
    - Updated App.tsx to integrate the new subjects list page and data provider.

---
+++ src/data/mockSubjects.ts
@@ -0,0 +1,25 @@
+import { Subject } from "../types";
+
+export const mockSubjects: Subject[] = [
+  {
+    id: 1,
+    code: "CS101",
+    name: "Introduction to Computer Science",
+    department: "Computer Science",
+    description: "An introductory course covering the basics of computer science, including algorithms, data structures, and programming fundamentals.",
+  },
+  {
+    id: 2,
+    code: "MATH201",
+    name: "Calculus II",
+    department: "Mathematics",
+    description: "Continuation of Calculus I, focusing on integration techniques, sequences and series, and parametric equations.",
+  },
+  {
+    id: 3,
+    code: "PHY102",
+    name: "General Physics I",
+    department: "Physics",
+    description: "A foundational course in physics covering mechanics, kinship, and energy principles for science and engineering students.",
+  },
+];
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,32 @@
 import { createSimpleRestDataProvider } from '@refinedev/rest/simple-rest';
 import { API_URL } from './constants';
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+import { DataProvider,GetListParams,GetListResponse,BaseRecord } from '@refinedev/core';
+import { m } from 'node_modules/react-router/dist/development/index-react-server-client-IoJGLOqV.d.mts';
+import { mockSubjects } from '@/data/mockSubjects';
+// export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
+//   apiURL: API_URL,
+// });
+
+
+
+export const dataProvider: DataProvider = {
+  getList: async <TData extends BaseRecord = BaseRecord>({ resource }: GetListParams): Promise<GetListResponse<TData>> => {
+    if (resource !== 'subjects') {
+      return {
+        data: [] as TData[],
+        total: 0
+      }     
+    }
+    return {
+      data: mockSubjects as unknown as TData[],
+      total: mockSubjects.length,
+    }
+  },
+
+  getOne: async () => {throw new Error('This function is not present in mock')},
+  create: async () => {throw new Error('This function is not present in mock')},
+  update: async () => {throw new Error('This function is not present in mock')},
+  deleteOne: async () => { throw new Error('This function is not present in mock') },
+  
+  getApiUrl: () => ''
+}
